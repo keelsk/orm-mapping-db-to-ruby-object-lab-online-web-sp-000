dd269e611ea9017cad0228eb69f8bfbb0be4b10b
Kirsten Keels <keelsk@gmail.com>
2020-03-19 15:40:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   
   def self.new_from_db(row)
     student = self.new
-    student.id = [0]
+    student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
     student
