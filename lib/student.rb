81e1a0bfc4e74a380b51392fd72de11057fd8d92
Kirsten Keels <keelsk@gmail.com>
2020-03-19 15:56:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,4 +97,17 @@ class Student
     end
   end
   
+  def self.first_X_students_in_grade_10(limit)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
