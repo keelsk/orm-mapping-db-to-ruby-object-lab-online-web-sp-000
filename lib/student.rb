2acdc40d860d918f8c8a75843acdcdc8f21fe16f
Kirsten Keels <keelsk@gmail.com>
2020-03-19 15:48:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,4 +73,16 @@ class Student
     end
   end
   
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute.map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
