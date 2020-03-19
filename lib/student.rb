14ff9767198f1d29b5d30c90a6cfe69c59f9c7d7
Kirsten Keels <keelsk@gmail.com>
2020-03-19 15:42:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,4 +48,19 @@ class Student
     student.grade = row[2]
     student
   end
+  
+  def self.find_by_name(name)
+    sql =  <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  
 end
